555d1e1efcf13b7f62e24cf2e27d8a2a9519156f
Hammad Asghar <qma1470353@gmail.com>
2026-06-15 23:10:46 +0500

    feat: initialize Spring Boot project with MySQL and Thymeleaf configuration

---
+++ src/main/java/com/mentalhealth/stresstracker/StresstrackerApplication.java
@@ -0,0 +1,11 @@
+package com.mentalhealth.stresstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StresstrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StresstrackerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mentalhealth/stresstracker/StresstrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentalhealth.stresstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StresstrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
